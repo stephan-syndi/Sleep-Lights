bbc8336f0fb248fee12fc71831f2f88d49ad8463
Stephan <31998878+silent97vvaaavava@users.noreply.github.com>
2025-10-28 18:41:16 -0700

    Initial Commit

---
+++ Sleep Lights/Sleep_LightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_LightsApp.swift
+//  Sleep Lights
+//
+//  Created by Stepan Degtsiaryk on 28.10.25.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_LightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
