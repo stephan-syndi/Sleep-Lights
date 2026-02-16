7817282efe26d6f6ae46600873d06b0715c76164
Stephan <stepandegtsiaryk@syndi-g.com>
2026-02-16 15:26:10 +0300

    add frw

---
+++ DarkCoreFramework.xcframework/ios-arm64/DarkCoreFramework.framework/Headers/DarkCoreFramework.h
@@ -0,0 +1,16 @@
+//
+//  DarkCoreFramework.h
+//  DarkCoreFramework
+//
+//  Created on 15.01.26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DarkCoreFramework.
+FOUNDATION_EXPORT double DarkCoreFrameworkVersionNumber;
+
+//! Project version string for DarkCoreFramework.
+FOUNDATION_EXPORT const unsigned char DarkCoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarkCoreFramework/PublicHeader.h>
